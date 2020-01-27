d14b356bac7b772b4f8c8af8aa9f9fd647d3bb58
11marc13 <59967311+11marc13@users.noreply.github.com>
2020-01-26 22:07:48 -0500

    hid/removed code not needed

--- Marc Agnello Web 3/Marc Agnello Web 3/dev/js/burger/burger.js
+++ Marc Agnello Web 3/Marc Agnello Web 3/dev/js/burger/burger.js
@@ -1,13 +1,5 @@
 var canYouSeeNav = false;
-// hide outline
-// gsap.set("#main-circle",{alpha:0});
-// gsap.set("#record-circle", {alpha:0});
 
-// gsap.set("#top-line", {rotate: 90, x: 5, y: 0});
-// gsap.set("#middle-line", {rotate: 90, y:0});
-// gsap.set("#bottom-line", {rotate: 90, x: -5, y:0});
-// reset the roation pin for the lines
-// gsap.set(".burger-lines",{transformOrigin:"50% 50%"});
 
 $( document ).ready(function() {
   console.log( "ready!" );
--- Marc Agnello Web 3/Marc Agnello Web 3/dev/js/scripts.js
+++ Marc Agnello Web 3/Marc Agnello Web 3/dev/js/scripts.js
@@ -5,26 +5,3 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 });
-
-
-//burger pre enter
-// gsap.set("#main-circle",{alpha:0});
-// gsap.set("#record-circle", {alpha:0});
-// var canYouSeeNav = false;
-
-//mouse mouseEnter
-// var lineToLine = gsap.timeline({paused:true});
-// lineToLine.to("#top-line",{duration:0.25, x:5},"move-lines")
-// .to("#bottom-line",{duration:0.25, y:-5},"move-lines")
-// .to("#middle-line", {alpha:0}, "move-lines");
-
-
-// $("#burger-container").on("mouseenter", function(){
-//     console.log("mouse enter");
-//
-//     if(canYouSeeNav === false){
-        // play the timeline to combine the lines into 1 line
-//         lineToLine.play();
-//     }
-//
-// });
