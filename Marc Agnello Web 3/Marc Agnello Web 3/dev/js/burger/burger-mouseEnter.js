ae6d2b98251fa182ab70849c1c5ac0cbf5e31153
11marc13 <59967311+11marc13@users.noreply.github.com>
2020-01-28 11:17:24 -0500

    changed record player nav
    changed to rotate on hover

--- Marc Agnello Web 3/Marc Agnello Web 3/dev/js/burger/burger-mouseEnter.js
+++ Marc Agnello Web 3/Marc Agnello Web 3/dev/js/burger/burger-mouseEnter.js
@@ -1,8 +1,13 @@
 var lineToLine = gsap.timeline({paused:true});
+var rotate = gsap.timeline({paused:true});
+
+
+
 lineToLine.to("#left-line",{duration:0.05, morphSVG: "#record-circle"}, "morph")
 .to("#right-line",{duration:0.25, morphSVG: "#record-line"}, "morph")
 .to("#middle-line",{duration:0.25, morphSVG: "#record-circle-small"}, "morph")
 .to("#record-dot", {alpha:1}, "morph")
+rotate.to("#spin-record",{transformOrigin: "14 4", rotate: 360, repeat: -1, ease: Power0.easeNone})
 
 $("#burger-container").on("mouseenter", function(){
     console.log("mouse enter");
@@ -10,6 +15,7 @@ $("#burger-container").on("mouseenter", function(){
     if(canYouSeeNav === false){
         // play the timeline to combine the lines into 1 line
         lineToLine.play();
+        rotate.play();
     }
 
 });
